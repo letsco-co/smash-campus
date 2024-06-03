4cfbc37a28506045c429f50c40c592a59b999443
Auriane <auriane.chay@letsco.co>
2024-06-03 10:10:16 +0200

    feat: required Title for TrainingEvaluationMethod. closes #20

--- app/src/Model/Training/TrainingEvaluationMethod.php
+++ app/src/Model/Training/TrainingEvaluationMethod.php
@@ -3,6 +3,8 @@
 namespace LetsCo\Model;
 
 use LetsCo\Trait\LocalizationDataObject;
+use SilverStripe\Forms\CompositeValidator;
+use SilverStripe\Forms\RequiredFields;
 use SilverStripe\ORM\DataObject;
 
 class TrainingEvaluationMethod extends DataObject
@@ -15,4 +17,12 @@ class TrainingEvaluationMethod extends DataObject
     private static $belongs_many_many = [
         'Trainings' => Training::class,
     ];
+    public function getCMSCompositeValidator(): CompositeValidator
+    {
+        $validator = parent::getCMSCompositeValidator();
+        $validator->addValidator(RequiredFields::create([
+            'Title',
+        ]));
+        return $validator;
+    }
 }
