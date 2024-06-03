82d80098b3c0b7cdb14358e48eaa2e7fad760955
Auriane <auriane.chay@letsco.co>
2024-06-03 23:35:16 +0200

    feat: display domains' trainings #10

--- app/src/PageType/TrainingHolderController.php
+++ app/src/PageType/TrainingHolderController.php
@@ -2,12 +2,16 @@
 
 namespace LetsCo\PageType;
 
+use LetsCo\Model\Training\Training;
 use LetsCo\Model\Training\TrainingCategory;
 use PageController;
 use SilverStripe\Control\HTTPRequest;
 
 class TrainingHolderController extends PageController
 {
+    private static $allowed_actions = [
+        'domain'
+    ];
     public function index(HTTPRequest $request) {
         $categories = TrainingCategory::get();
         if (!$categories->exists()) {
@@ -17,6 +21,25 @@ class TrainingHolderController extends PageController
             'Layout' => $this
                 ->customise(['categories' => $categories])
                 ->renderWith(['TrainingHolder']),
+        ])->renderWith(['Page']);
+    }
+
+    public function domain(HTTPRequest $request) {
+        $domainID = $request->param('ID');
+        $domain = TrainingCategory::get()->filter('URLSegment', $domainID)->first();
+
+        if (!$domain) {
+            return $this->httpError(404, 'Domain not found');
+        }
+        $trainings = Training::get()->filter('CategoryID', $domain->ID);
+
+        return $this->customise([
+            'Layout' => $this
+            ->customise([
+                'Trainings' => $trainings,
+                'Domain' => $domain
+            ])
+            ->renderWith(['TrainingHolder_domain']),
         ])->renderWith(['Page']);
     }
 }
