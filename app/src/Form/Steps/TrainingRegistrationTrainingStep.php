d42ecb19749c7c4da66a89817de1549e16a0f067
Auriane <auriane.chay@letsco.co>
2024-06-07 16:08:12 +0200

    fix: #5 #32 reduce textarea rows

--- app/src/Form/Steps/TrainingRegistrationTrainingStep.php
+++ app/src/Form/Steps/TrainingRegistrationTrainingStep.php
@@ -24,7 +24,7 @@ class TrainingRegistrationTrainingStep extends MultiFormStep
         $fields = FieldList::create(
             HeaderField::create('Training', _t(TrainingRegistration::class.'.TrainingInfos', 'Training Infos'), 3),
             TextField::create('DesiredTrainingDate', _t(TrainingRegistration::class.'.DesiredTrainingDate', 'DesiredTrainingDate'))->addExtraClass("form-control"),
-            TextareaField::create('Reasons', _t(TrainingRegistration::class.'.Reasons', 'Reasons'))->addExtraClass("form-control"),
+            TextareaField::create('Reasons', _t(TrainingRegistration::class.'.Reasons', 'Reasons'))->addExtraClass("form-control")->setRows(1),
             CheckboxSetField::create('Financing', _t(TrainingRegistration::class.'.Financing', 'Financing'), TrainingRegistration::getTranslatableEnumValues($trainingRegistration->dbObject('Financing')->enumValues())),
             OptionsetField::create('IsDisabled', _t(TrainingRegistration::class.'.IsDisabled', 'IsDisabled'), TrainingRegistration::getTranslatableEnumValues($trainingRegistration->dbObject('IsDisabled')->enumValues())),
             CheckboxSetField::create('HeardOfTrainingSource', _t(TrainingRegistration::class.'.HeardOfTrainingSource', 'HeardOfTrainingSource'), TrainingRegistration::getTranslatableEnumValues($trainingRegistration->dbObject('HeardOfTrainingSource')->enumValues())),
