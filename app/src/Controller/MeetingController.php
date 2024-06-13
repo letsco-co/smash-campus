7b737ae0d26dbe7be84960dab024f908902685c4
Auriane <auriane.chay@letsco.co>
2024-06-13 14:51:35 +0200

    feat: #3 Meeting display images as carrousel

--- app/src/Controller/MeetingController.php
+++ app/src/Controller/MeetingController.php
@@ -8,6 +8,7 @@ use LetsCo\Model\Training\Training;
 use SilverStripe\CMS\Controllers\ContentController;
 use SilverStripe\Control\Controller;
 use SilverStripe\Control\HTTPRequest;
+use SilverStripe\View\Requirements;
 
 class MeetingController extends ContentController
 {
@@ -26,6 +27,13 @@ class MeetingController extends ContentController
 
     public function index(HTTPRequest $request)
     {
+        Requirements::javascript('https://unpkg.com/@glidejs/glide@3.6.1/dist/glide.js');
+        Requirements::javascript('themes/smash-campus/javascript/carrousel.js', [
+            'type' => 'module'
+        ]);
+        Requirements::css('https://unpkg.com/@glidejs/glide@3.6.1/dist/css/glide.core.min.css');
+        Requirements::css('https://unpkg.com/@glidejs/glide@3.6.1/dist/css/glide.theme.css');
+
         $meeting = Meeting::get()->filter('URLSegment',$request->param('Action'))->first();
 
         if(!$meeting) {
