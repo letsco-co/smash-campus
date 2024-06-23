07a62f6ee177f11715fd59592b379e80306f4a6c
Auriane <auriane.chay@letsco.co>
2024-06-23 22:19:34 +0200

    feat: translate domainholder

--- app/src/PageType/DomainHolder.php
+++ app/src/PageType/DomainHolder.php
@@ -4,6 +4,7 @@ namespace LetsCo\PageType;
 
 class DomainHolder extends \Page
 {
+    private static $description = 'Parent page to display domains';
     private static $allowed_children = [
         DomainPage::class
     ];
