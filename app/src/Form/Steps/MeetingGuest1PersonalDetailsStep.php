b4d2391d9a6697554dc1b41936f81e669bfc5da8
Auriane <auriane.chay@letsco.co>
2024-06-24 17:23:41 +0200

    fix: form indication display not correct

--- app/src/Form/Steps/MeetingGuest1PersonalDetailsStep.php
+++ app/src/Form/Steps/MeetingGuest1PersonalDetailsStep.php
@@ -10,4 +10,14 @@ class MeetingGuest1PersonalDetailsStep extends MultiFormStep
     use MeetingGuestFields;
     private static $next_steps = MeetingGuest2PersonalDetailsStep::class;
 
+    public function getNextStep()
+    {
+        $number = $this->getValueFromOtherStep(MeetingGuestNumberStep::class, 'NumberGuests');
+        if(isset($number) && $number == 2) {
+            $this->config()->set('next_steps',MeetingGuest3PersonalDetailsStep::class);
+            return MeetingGuest3PersonalDetailsStep::class;
+        } else {
+            return $this->config()->get('next_steps');
+        }
+    }
 }
