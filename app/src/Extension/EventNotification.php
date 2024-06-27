9c54442726b8532fc2feb1b8061ac30c11473332
Auriane <auriane.chay@letsco.co>
2024-06-27 08:59:01 +0200

    feat: do not display ListId field

--- app/src/Extension/EventNotification.php
+++ app/src/Extension/EventNotification.php
@@ -5,6 +5,7 @@ namespace LetsCo\Extension;
 use LetsCo\Email\BrevoEmailProvider;
 use LetsCo\Model\Training\Training;
 use SilverStripe\Core\Environment;
+use SilverStripe\Forms\FieldList;
 use SilverStripe\ORM\DataExtension;
 
 class EventNotification extends DataExtension
@@ -12,6 +13,11 @@ class EventNotification extends DataExtension
     private static $db = [
         'ListId' => "Int"
     ];
+
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fields->removeByName('ListId');
+    }
 
     public function onAfterPublish(&$original)
     {
