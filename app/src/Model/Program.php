44c2af8eaf6e3619c9b37b3f761c4960239e2cdc
Auriane <auriane.chay@letsco.co>
2024-07-05 17:24:16 +0200

    feat: better display of the program field in Training and Meeting

--- app/src/Model/Program.php
+++ app/src/Model/Program.php
@@ -3,11 +3,22 @@
 namespace LetsCo\Model;
 
 use LetsCo\Trait\LocalizationDataObject;
+use SilverStripe\Forms\CompositeField;
 use SilverStripe\Forms\CompositeValidator;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldButtonRow;
+use SilverStripe\Forms\GridField\GridFieldConfig;
+use SilverStripe\Forms\GridField\GridFieldDeleteAction;
+use SilverStripe\Forms\GridField\GridFieldToolbarHeader;
+use SilverStripe\Forms\LiteralField;
 use SilverStripe\Forms\RequiredFields;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\ORM\Hierarchy\Hierarchy;
 use SilverStripe\View\Parsers\URLSegmentFilter;
+use Symbiote\GridFieldExtensions\GridFieldAddNewInlineButton;
+use Symbiote\GridFieldExtensions\GridFieldEditableColumns;
+use Symbiote\GridFieldExtensions\GridFieldTitleHeader;
 
 class Program extends DataObject
 {
@@ -18,14 +29,26 @@ class Program extends DataObject
     ];
     private static $has_one = [
         'Event' => Event::class,
+        'Parent' => Program::class,
+    ];
+
+    private static $has_many = [
+        'Children' => Program::class,
     ];
     private static $summary_fields = [
         'Title',
-        'Parent.Title'
+        'ChildrenList'
     ];
-    private static $extensions = [
-        Hierarchy::class,
+
+    private static $casting = [
+        'ChildrenList' => 'Varchar',
     ];
+
+    public function getChildrenList()
+    {
+        $list = array_values($this->Children()->map()->toArray());
+        return implode(', ', $list);
+    }
 
     public function getCMSCompositeValidator(): CompositeValidator
     {
@@ -50,5 +73,37 @@ class Program extends DataObject
         $this->extend('updateURLSegment', $filteredTitle, $title);
 
         return $filteredTitle;
+    }
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->removeByName('ParentID');
+        $fields->removeByName('Children');
+        $fields->dataFieldByName('EventID')->setDisabled(true);
+        $fields->insertBefore('EventID',LiteralField::create('Parts_Title' . 'Title', '<label >' . _t(self::class . '.' . 'Parts', 'Parts') . '</label>'));
+        $config = 	GridFieldConfig::create()
+            ->addComponent(GridFieldButtonRow::create('before'))
+            ->addComponent(GridFieldToolbarHeader::create())
+            ->addComponent(GridFieldTitleHeader::create())
+            ->addComponent(GridFieldEditableColumns::create())
+            ->addComponent(GridFieldDeleteAction::create())
+            ->addComponent(GridFieldAddNewInlineButton::create());
+        $config->getComponentByType(GridFieldEditableColumns::class)->setDisplayFields([
+            'Title' => _t(self::class.'.SubTitle', 'Sub Title')
+        ]);
+        $fields->insertBefore('EventID',CompositeField::create(new FieldList([
+            GridField::create('Programs', false, $this->Children(), $config),
+        ])));
+        return $fields;
+    }
+
+    protected function onBeforeWrite()
+    {
+        parent::onBeforeWrite();
+        if ($this->ParentID)
+        {
+            $this->EventID = $this->Parent()->EventID;
+        }
     }
 }
