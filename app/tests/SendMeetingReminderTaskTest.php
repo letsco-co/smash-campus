ffb988e33284a1c86e12e96c9ba3072682ba6a57
Auriane <auriane.chay@letsco.co>
2024-09-10 09:56:12 +0200

    feat: only instanciate reminderTask once

--- app/tests/SendMeetingReminderTaskTest.php
+++ app/tests/SendMeetingReminderTaskTest.php
@@ -9,11 +9,17 @@ class SendMeetingReminderTaskTest extends SapphireTest
 {
     protected static $fixture_file = 'SendMeetingReminderTaskTest.yml';
 
+    protected SendMeetingReminderTask $reminderTask;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->reminderTask = new SendMeetingReminderTask();
+    }
 
     public function testGetNoMeetingWhenNoneIn7Days()
     {
-        $reminderTask = new SendMeetingReminderTask();
-        $meetings = $reminderTask->getMeetingRegistrationsForMeetingHappeningThatDay('2024-06-30');
+        $meetings = $this->reminderTask->getMeetingRegistrationsForMeetingHappeningThatDay('2024-06-30');
         $this->assertCount(0,$meetings);
     }
 }
